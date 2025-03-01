92d83b55b6b1cd9ee123f078f13445469483694f
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-01 13:51:50 +0530

    UC1 committed

---
+++ springconcepts/src/main/java/com/demo/springconcepts/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.springconcepts;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+}
